22a035fc4ea4bb436b4f2ffee2239019206b62ed
elizabethvera-atieno <atienolizvera@gmail.com>
2022-06-13 20:43:25 +0300

    commit tree d7987606a635d077739c1b77b3a83a80146bc36f

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+require_relative './user'
+
+class Student < User
+  attr_accessor :knowledge
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn learn
+    @knowledge << learn.to_s
+  end
+  def knowledge
+    @knowledge
+  end
 
 end
